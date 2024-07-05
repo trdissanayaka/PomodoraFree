806185110d6fde8e8e3973338790146dcb0ba5bc
TR Dissanayaka <thilinda.roshan@gmail.com>
2024-07-05 10:36:12 +0530

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,15 @@
+namespace Pomodora
+{
+    public partial class formMain : Form
+    {
+        public formMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
